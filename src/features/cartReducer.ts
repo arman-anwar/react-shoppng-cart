d68e9f07d9e3e6dd4ec3fd0d85be9b8fe94b62fb
Arman <arman.anwar@gmail.com>
2022-11-11 07:03:08 +0000

    reducer functions

--- src/features/cartReducer.ts
+++ src/features/cartReducer.ts
@@ -10,13 +10,25 @@ const cartSlice = createSlice({
     initialState,
     reducers: {
         addCartItem: (state, action) => {
+            let item: CartItem = action.payload
+            state.cartItems.push(item)
         },
 
         increaseCartQuantity: (state, action) => {
+            let cartItem = state.cartItems.find(item => item.product.id === action.payload.id)
+            if (cartItem) {
+                cartItem.quantity++;
+                state.cartItems = [...state.cartItems];
+            } else {
+                let newCartItem: CartItem = { cartItemId: state.cartItems.length + 1, quantity: 1, product: action.payload }
+                state.cartItems = [...state.cartItems, newCartItem]
+            }
         },
         decreaseCartQuantity: (state, action) => {
         },
         removeFromCart: (state, action) => {
+            let filtered = state.cartItems.filter(ele => { return ele.product.id !== action.payload.id });
+            state.cartItems = [...filtered];
         }
     }
 });
